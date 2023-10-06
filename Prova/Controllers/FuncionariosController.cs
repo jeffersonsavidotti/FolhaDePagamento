5a51d4c22ac4d07692418a3ae6b5c4c9a638bd7a
jeffersonsavidotti <jeffersonsavidotti@gmail.com>
2023-10-06 20:44:23 -0300

    Adicionar arquivos de projeto.

---
+++ Prova/Controllers/FuncionariosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prova.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Nome"] = "Naiara";
+            ViewData["RA"] = "Matricula: G545648";
+            ViewData["Cargo"] = "Desenvolvedora Web";
+            return View();
+        }
+
+        public IActionResult Index1()
+        {
+            ViewData["Nome"] = "Jefferson";
+            ViewData["RA"] = "Matricula: N723254";
+            ViewData["Cargo"] = "Desenvolvedor Back-end";
+            return View();
+        }
+    }
+}
